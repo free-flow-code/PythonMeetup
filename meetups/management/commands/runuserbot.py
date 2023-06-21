a4882cb667484de835614df91cc67675a7708959
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-06-21 15:36:26 +0300

    Add user registration to  userbot

--- meetups/management/commands/runuserbot.py
+++ meetups/management/commands/runuserbot.py
@@ -1,7 +1,15 @@
 import logging
 
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
 from django.core.management.base import BaseCommand
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from meetups.models import(
+    Client,
+)
+from asgiref.sync import sync_to_async
 from conf import settings
 
 logging.basicConfig(
@@ -11,16 +19,62 @@ logging.basicConfig(
 
 logger = logging.getLogger('UserBot')
 
+storage = MemoryStorage()
 bot = Bot(settings.TG_TOKEN_API)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot=bot, storage=storage)
+
+user_register_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='Зарегистрироваться', callback_data='user_register'),
+    ],
+])
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    if message.text.count(' ') >= 1:
-        await message.answer(text=message.text.upper())
+class ClientRegisterFSM(StatesGroup):
+    personal_info = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message) -> None:
+    client, _ = await sync_to_async(Client.objects.get_or_create)(
+        chat_id=message.from_user.id,
+    )
+    await message.answer('Добро пожаловать! Чтобы начать работу с ботом необходимо зарегистрироваться.',
+                         parse_mode='HTML',
+                         reply_markup=user_register_keyboard,
+                         )
+
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'user_register')
+async def user_register_handler(callback: types.CallbackQuery) -> None:
+    await ClientRegisterFSM.personal_info.set()
+    await callback.message.edit_text('Введите ваше имя и фамилию в формате: <b>Имя Фамилия</b>',
+                                     parse_mode='HTML',
+                                    )
+
+
+@dp.message_handler(state=ClientRegisterFSM.personal_info)
+async def user_register_personal_info_handler(message: types.Message, state: FSMContext) -> None:
+    if message.text.count(' ') != 1:
+        await message.answer('Неверный формат ввода. Попробуйте еще раз.\n'
+                             'Введите ваше имя и фамилию в формате: <b>Имя Фамилия</b>',
+                             parse_mode='HTML',
+                             )
+        return
+    first_name, last_name = message.text.split()
+    client, _ = await sync_to_async(Client.objects.get_or_create)(
+        chat_id=message.from_user.id,
+        defaults={
+            'first_name': first_name,
+            'last_name': last_name,
+        },
+    )
+    await bot.send_message(client.chat_id,
+                           f'Вы успешно зарегистрированы!',
+                         parse_mode='HTML',
+                         )
+    await state.finish()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        executor.start_polling(dp)
+        executor.start_polling(dp, skip_updates=True)
--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,89 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    chat_id = models.CharField(max_length=20, verbose_name='ID чата клиента')
+    first_name = models.CharField(max_length=40, verbose_name='Имя клиента', null=True, blank=True)
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия клиента", null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name="Создано", auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self):
+        return f'{self.chat_id}: {self.first_name} {self.last_name}'
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название мероприятия')
+    date = models.DateField(verbose_name='Дата мероприятия')
+    start_time = models.TimeField(verbose_name='Время начала мероприятия')
+    visitors = models.ManyToManyField(
+        Client,
+        through='Visitor',
+        related_name='events',
+        verbose_name='Посетители',
+    )
+
+    class Meta:
+        verbose_name = 'Мероприятие'
+        verbose_name_plural = 'Мероприятия'
+
+    def __str__(self):
+        return f'{self.name} {self.date} {self.start_time}'
+
+
+class Visitor(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Клиент')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие')
+    created_at = models.DateTimeField(verbose_name='Записан', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Посетитель'
+        verbose_name_plural = 'Посетители'
+
+    def __str__(self):
+        return f' {self.event.name}: {self.client.first_name} {self.client.last_name}'
+
+
+class Presentation(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название презентации')
+    annotation = models.TextField(verbose_name='Аннотация')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие')
+    start_time = models.TimeField(verbose_name='Время начала презентации')
+    end_time = models.TimeField(verbose_name='Время окончания презентации')
+    speaker = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Спикер', related_name='presentations')
+
+    class Meta:
+        verbose_name = 'Презентация'
+        verbose_name_plural = 'Презентации'
+
+    def __str__(self):
+        return f'{self.name}: {self.event.name}'
+
+
+class Question(models.Model):
+    text = models.TextField(verbose_name='Текст вопроса')
+    presentation = models.ForeignKey(Presentation, on_delete=models.CASCADE, verbose_name='Презентация', related_name='questions')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Клиент', related_name='questions')
+    created_at = models.DateTimeField(verbose_name='Задан', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+    def __str__(self):
+        return f'{self.text}, ({self.client.first_name} {self.client.last_name})'
+
+class Likes(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Вопрос', related_name='likes')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Клиент', related_name='likes')
+    created_at = models.DateTimeField(verbose_name='Поставлен', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Лайк'
+        verbose_name_plural = 'Лайки'
+
+    def __str__(self):
+        return f'{self.question.text}: {self.client.first_name} {self.client.last_name}'
